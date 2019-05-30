d9d51d83694c44225121ab4e0a058854dcbef742
Jean <jean@Jeans-MacBook-Pro.local>
2019-05-30 09:37:50 -0700

    Adding database model and DDL script

---
+++ docs/database/Language Classes_mysql_create.sql
@@ -0,0 +1,118 @@
+CREATE TABLE `user` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`email` varchar(100) NOT NULL,
+	`password` varchar(255) NOT NULL,
+	`user_type` char NOT NULL DEFAULT 'S',
+	`name` varchar(255) NOT NULL,
+	`city_id` INT,
+	`birth_date` DATE,
+	`bio` TEXT,
+	`mother_country_id` INT,
+	`teacher_price` FLOAT,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `class` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`student_id` INT NOT NULL,
+	`teacher_id` INT NOT NULL,
+	`date` TIMESTAMP NOT NULL,
+	`duration` TIME NOT NULL,
+	`class_type_id` INT NOT NULL,
+	`location` varchar(255),
+	`price` FLOAT NOT NULL,
+	`class_completed` bool NOT NULL,
+	`rating_to_student` tinyint,
+	`rating_to_teacher` tinyint,
+	`review_to_teacher` TEXT,
+	`review_to_student` TEXT,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `city` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL UNIQUE,
+	`country_id` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `favorite_teacher` (
+	`student_id` INT NOT NULL,
+	`teacher_id` INT NOT NULL,
+	`id` INT NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `language` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` varchar(50) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `teacher_language` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`teacher_id` INT NOT NULL,
+	`language_id` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `class_type` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` varchar(50) NOT NULL,
+	`desc` varchar(255) NOT NULL,
+	`teacher_id` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `message` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`sender_id` INT NOT NULL,
+	`receiver_id` INT NOT NULL,
+	`date` TIMESTAMP NOT NULL,
+	`message` TEXT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `teacher_availability` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`teacher_id` INT NOT NULL,
+	`day_of_week` INT NOT NULL,
+	`begin` TIME NOT NULL,
+	`end` TIME NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `country` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL UNIQUE,
+	PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `user` ADD CONSTRAINT `user_fk0` FOREIGN KEY (`city_id`) REFERENCES `city`(`id`);
+
+ALTER TABLE `user` ADD CONSTRAINT `user_fk1` FOREIGN KEY (`mother_country_id`) REFERENCES `country`(`id`);
+
+ALTER TABLE `class` ADD CONSTRAINT `class_fk0` FOREIGN KEY (`student_id`) REFERENCES `user`(`id`);
+
+ALTER TABLE `class` ADD CONSTRAINT `class_fk1` FOREIGN KEY (`teacher_id`) REFERENCES `user`(`id`);
+
+ALTER TABLE `class` ADD CONSTRAINT `class_fk2` FOREIGN KEY (`class_type_id`) REFERENCES `class_type`(`id`);
+
+ALTER TABLE `city` ADD CONSTRAINT `city_fk0` FOREIGN KEY (`country_id`) REFERENCES `country`(`id`);
+
+ALTER TABLE `favorite_teacher` ADD CONSTRAINT `favorite_teacher_fk0` FOREIGN KEY (`student_id`) REFERENCES `user`(`id`);
+
+ALTER TABLE `favorite_teacher` ADD CONSTRAINT `favorite_teacher_fk1` FOREIGN KEY (`teacher_id`) REFERENCES `user`(`id`);
+
+ALTER TABLE `teacher_language` ADD CONSTRAINT `teacher_language_fk0` FOREIGN KEY (`teacher_id`) REFERENCES `user`(`id`);
+
+ALTER TABLE `teacher_language` ADD CONSTRAINT `teacher_language_fk1` FOREIGN KEY (`language_id`) REFERENCES `language`(`id`);
+
+ALTER TABLE `class_type` ADD CONSTRAINT `class_type_fk0` FOREIGN KEY (`teacher_id`) REFERENCES `user`(`id`);
+
+ALTER TABLE `message` ADD CONSTRAINT `message_fk0` FOREIGN KEY (`sender_id`) REFERENCES `user`(`id`);
+
+ALTER TABLE `message` ADD CONSTRAINT `message_fk1` FOREIGN KEY (`receiver_id`) REFERENCES `user`(`id`);
+
+ALTER TABLE `teacher_availability` ADD CONSTRAINT `teacher_availability_fk0` FOREIGN KEY (`teacher_id`) REFERENCES `user`(`id`);
+
